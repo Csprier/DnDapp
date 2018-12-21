589f7d9275cdb4d79a2e04a40c95de32ca262343
Cam Prier <CSPrier@gmail.com>
2018-12-20 19:31:15 -0500

    Added show/hide functionality on gameMechnics.js

--- src/components/gameMechanics.js
+++ src/components/gameMechanics.js
@@ -4,14 +4,37 @@ import Conditions from './conditions';
 import DamageTypes from './damageTypes';
 import MagicSchools from './magicSchools'
 
+import '../css/gameMechanics.css';
+
 class GameMechanics extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      display: false
+    }
+  }
+
+  toggleDisplay = () => {
+    console.log('Toggle display button clicked. Changing the state from:', this.state.display);
+    this.setState({
+      display: !this.state.display
+    });
+    // console.log('this.setState has occurred in toggleDisplay()');
+  }
+
   render() {
+    console.log('Display state is now: ', this.state.display);
     return (
       <div className="game-mechanics-info-container">
+      <button onClick={() =>this.toggleDisplay()}>
         <h2>Game Mechanics</h2>
-        <Conditions />
-        <DamageTypes />
-        <MagicSchools />
+      </button>
+        <div className={(this.state.display) ? "expanded" : "hidden"}>
+          <Conditions />
+          <DamageTypes />
+          <MagicSchools />
+        </div>
       </div>
     );
   }
